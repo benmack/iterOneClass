5a66ad911f04dbf8316c362d4b334a4f54218ff7
benmack <knecmab@gmail.com>
2014-12-22 08:44:31 +0100

    ...

--- R/get_full_pred_vector.R
+++ R/get_full_pred_vector.R
@@ -1,6 +1,6 @@
 #'@export
 get_full_pred_vector <- function(ioccObj){
-    pred_iocc_all = (pred*0 + min(ioccObj$pred)) - 
+    pred_iocc_all = (ioccObj$pred*0 + min(ioccObj$pred)) - 
     abs(min(ioccObj$pred))*.05
     pred_iocc_all = jitter(pred_iocc_all)
     pred_iocc_all[ioccObj$pred_neg==0] = ioccObj$pred
--- R/one_step_occ.R
+++ R/one_step_occ.R
@@ -1,17 +1,21 @@
+#'@export
 one_step_occ <- function(sttngs, iter, filename_U, test_set,
-                         folder_out, allowParallel=TRUE) {
+                         folder_out, allowParallel=TRUE,
+                         modRows = 0) {
   
   time_stopper_model <- time_stopper_predict <- c()
   
   fname_model <- paste0(folder_out, "iter-", iter, "_model.RData")
   dir.create(folder_out)
+  fname_plot = paste0(folder_out, "iter-", iter, "_WHAT.pdf")
   
-  
-  iocc.i <- iocc_load(sttngs$baseDir, iter,
-                      filename_U=filename_U)
+  iocc.i <- iocc_load(sttngs$baseDir, iter, U_as_df=FALSE, filename_U=filename_U)
   un <- rasterTiled(brick(iocc.i$filename_U), mask=iocc.i$validCells)
   idx.feats <- colnames(iocc.i$model$trainingData)!=".outcome"
   names(un$raster) <- colnames(iocc.i$model$trainingData)[idx.feats]
+  
+
+  
   
   if (!file.exists(fname_model)) {
     
@@ -36,73 +40,95 @@ one_step_occ <- function(sttngs, iter, filename_U, test_set,
     time_stopper_model <- rbind(time_stopper_model, (proc.time()-ans)[1:3])
     save(train_pu_x, train_pu_y, n_un_bsvm, index, model, 
          time_stopper_model, file=fname_model)
+    
+    
+    pdf(file=gsub("WHAT", "model_grid_puF", fname_plot))
+      trellis.par.set(caretTheme())
+      p = plot(model, plotType="level", metric="puF")
+      print(p)
+      dev.off()
+    pdf(file=gsub("WHAT", "model_grid_puAuc", fname_plot))
+      trellis.par.set(caretTheme())
+      p = plot(model, plotType="level", metric="puAuc")
+      print(p)
+    dev.off()
+    
   } else {
     load(fname_model)
   }
   
-  modRow <- modelPosition(model)$row
-  fname_pred <- gsub("model", paste0("model-", modRow, "_pred"),
-                     fname_model)
+  if (modRows == 0) {
+    modRow_maxPuAuc <- modelPosition(model, modRank=1, by="puAuc")$row
+    modRow_maxPuF <- modelPosition(model, modRank=1, by="puF")$row
+    modRows <- unique(c(modRow_maxPuAuc, modRow_maxPuF))
+    cat(paste("Running models:", paste(modRows, collapse=", "), "\n"))
+  }
   
-  if (!file.exists(fname_pred)) {
+  for (modRow in modRows) {
     
-    cat("\tPrediction ... ")
-    ans <- proc.time()
-    pred <- predict(un, model, returnRaster = FALSE, 
-                    fnameLog = gsub(".RData", "_LOG.txt", 
-                                    fname_pred) )
-    time_stopper_predict <- rbind(time_stopper_predict, (proc.time()-ans)[1:3])
-    save(pred, time_stopper_predict, file=fname_pred)
-  } else {
-    load(fname_pred)
-  }
-  
-  if (!is.null(test_set)) {
-    fname_eval <- gsub("model", paste0("model-", modRow, "_eval"),
+    fname_pred <- gsub("model", paste0("model-", modRow, "_pred"),
                        fname_model)
-    if (!file.exists(fname_eval)) {
-      cat("\tEvaluation ... ")
-      pred_test <- predict(model, test_set[, -1])
-      ev <- evaluate(p = pred_test[test_set$y==1], 
-                     a = pred_test[test_set$y==-1])
-      save(pred_test, ev, file=fname_eval)
+    
+    if (!file.exists(fname_pred)) {
+      
+      cat("\tPrediction ... ")
+      ans <- proc.time()
+      pred <- predict(un, model, returnRaster = FALSE, 
+                      fnameLog = gsub(".RData", "_LOG.txt", 
+                                      fname_pred) )
+      time_stopper_predict <- rbind(time_stopper_predict, (proc.time()-ans)[1:3])
+      save(pred, time_stopper_predict, file=fname_pred)
     } else {
-      load(fname_eval)
+      load(fname_pred)
     }
+    
+    if (!is.null(test_set)) {
+      fname_eval <- gsub("model", paste0("model-", modRow, "_eval"),
+                         fname_model)
+      if (!file.exists(fname_eval)) {
+        cat("\tEvaluation ... ")
+        pred_test <- predict(model, test_set[, -1])
+        ev <- evaluate(p = pred_test[test_set$y==1], 
+                       a = pred_test[test_set$y==-1])
+        save(pred_test, ev, file=fname_eval)
+      } else {
+        load(fname_eval)
+      }
+    }
+    
+    ### Some comparison plots
+    try({
+      fname_plot = gsub("model", paste0("model-", modRow, "_WHAT"),
+                        fname_model)
+      fname_plot = gsub(".RData", ".pdf", fname_plot)
+      
+      ###
+      if (ncol(train_pu_x)==2){
+        pdf(file=gsub("WHAT", "featurespace_iocc", fname_plot))
+        featurespace(iocc.i$model)
+        dev.off()
+        pdf(file=gsub("WHAT", "featurespace_bench", fname_plot))
+        featurespace(model)
+        dev.off()
+      }
+      
+      ### Hist plot and ev of benchmark
+      pdf(file=gsub("WHAT", "histUev_bench", fname_plot))
+      yLimits = histUev(model, ev, pred=pred)
+      abline(h=max(yLimits)*max(iocc.i$ev@kappa), lwd=2)
+      dev.off()
+      ### Hist plot and ev of iocc
+      pdf(file=gsub("WHAT", "histUev_iocc", fname_plot))
+      yLimits = histUev(iocc.i$model, iocc.i$ev, pred=iocc.i$pred)
+      abline(h=max(yLimits)*max(ev@kappa), lwd=2)
+      dev.off()
+      
+      ### Comparing predictions
+      pred_iocc_all <- get_full_pred_vector(iocc.i)
+      hist2d(pred, pred_iocc_all, fname=gsub("WHAT", "hist2d", 
+                                             fname_plot))
+    })
   }
-  
-  ### Some comparison plots
-  fname_plot = gsub("model", paste0("model-", modRow, "_WHAT"),
-                    fname_model)
-  fname_plot = gsub(".RData", ".pdf", fname_plot)
-  
-  ###
-  if (ncol(train_pu_x)==2){
-    pdf(file=gsub("WHAT", "featurespace_iocc", fname_plot))
-    featurespace(iocc.i$model)
-    dev.off()
-    pdf(file=gsub("WHAT", "featurespace_bench", fname_plot))
-    featurespace(model)
-    dev.off()
-  }
-  
-  ### Hist plot and ev of benchmark
-  pdf(file=gsub("WHAT", "histUev_bench", fname_plot))
-  yLimits = histUev(model, ev, pred=pred)
-  abline(h=max(yLimits)*max(iocc.i$ev@kappa), lwd=2)
-  dev.off()
-  ### Hist plot and ev of iocc
-  pdf(file=gsub("WHAT", "histUev_iocc", fname_plot))
-  yLimits = histUev(iocc.i$model, iocc.i$ev, pred=iocc.i$pred)
-  abline(h=max(yLimits)*max(ev@kappa), lwd=2)
-  dev.off()
-  
-  ### Comparing predictions
-  pred_iocc_all <- get_full_pred_vector(iocc.i)
-  hist2d(pred, pred_iocc_all, fname=gsub("WHAT", "hist2d", 
-                                         fname_plot))
-  
-  
   
   #   ### AUCs - TEST PU
   #   ### This does not make a lot of sense, the curves are too similar!
